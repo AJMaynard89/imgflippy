df98441901df415cc4f52e0786ee7ddfb6e457ef
AJMaynard89 <AJMaynard89@gmail.com>
2018-03-04 21:27:24 -0500

    First release

--- flippy.py
+++ flippy.py
@@ -15,18 +15,11 @@ class Flippy:
 	GetMemesApi = 'https://api.imgflip.com/get_memes'
 	CaptionImageApi = 'https://api.imgflip.com/caption_image'
 
-	def __init__(self):
-		print("TEST")
-		print(self.GetMemesApi)
-
 	def genMeme(self, template_id, username, password, text0, text1, font="impact", max_font_size="50", boxes={}):
 		payload=dict([('template_id',template_id), ('username',username),
 		('password',password),('text0',text0),('text1',text1),
 		('font',font),('max_font_size',max_font_size),
 		('boxes',boxes)])
-		results = requests.post(CaptionImageApi,params=payload)
-		data = r.json()
-		print(data)
-
-flippy = Flippy()
-flippy.genMeme("Template","Username","Password","Text0","Text1")
+		results = requests.post(self.CaptionImageApi,params=payload)
+		data = results.json()
+		return(data["data"]["url"])
