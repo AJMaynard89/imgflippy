3060765fc3cda4eaf2acf69870ca9dbf81f4f82e
AJMaynard89 <AJMaynard89@gmail.com>
2018-03-04 21:02:28 -0500

    In progress

--- flippy.py
+++ flippy.py
@@ -10,16 +10,23 @@ import requests
 
 
 class Flippy:
-	GetMemesApi = "https://api.imgflip.com/get_memes"
-	CaptionImageApi = "https://api.imgflip.com/caption_image"
-	data = {}
-	def genMeme(template_id, username, password, text0, text1, font="impact", max_font_size="50", boxes={}):
+	"""MAIN FLIPPY CLASS"""
+
+	GetMemesApi = 'https://api.imgflip.com/get_memes'
+	CaptionImageApi = 'https://api.imgflip.com/caption_image'
+
+	def __init__(self):
+		print("TEST")
+		print(self.GetMemesApi)
+
+	def genMeme(self, template_id, username, password, text0, text1, font="impact", max_font_size="50", boxes={}):
 		payload=dict([('template_id',template_id), ('username',username),
-				('password',password),('text0',text0),('text1',text1),
-				('font',font),('max_font_size',max_font_size),
-				('boxes',boxes)])
-		print(payload)
+		('password',password),('text0',text0),('text1',text1),
+		('font',font),('max_font_size',max_font_size),
+		('boxes',boxes)])
+		results = requests.post(CaptionImageApi,params=payload)
+		data = r.json()
+		print(data)
 
 flippy = Flippy()
-flippy.genMeme("Template","Username","Password","Text0","Text1","Font")
-
+flippy.genMeme("Template","Username","Password","Text0","Text1")
