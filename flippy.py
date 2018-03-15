bd1bd799189525b4d24545e6d9ab4757c7649818
Alexander Maynard <almaynard@xpologistics.com>
2018-03-15 11:53:59 -0400

    Added function getAvailableMemes() that will return a JSON with template ids, names, and preview images of top memes.

--- flippy.py
+++ flippy.py
@@ -1,25 +1,22 @@
 import requests
 
-
-
-#one does not simply 61579
-#payload = {'template_id': '61579','text0':'LOL'}
-#r = requests.post(CaptionImageApi,params=payload)
-#data = r.json()
-#print(data)
-
-
 class Flippy:
-	"""MAIN FLIPPY CLASS"""
+    """MAIN FLIPPY CLASS"""
 
-	GetMemesApi = 'https://api.imgflip.com/get_memes'
-	CaptionImageApi = 'https://api.imgflip.com/caption_image'
+    GetMemesApi = 'https://api.imgflip.com/get_memes'
+    CaptionImageApi = 'https://api.imgflip.com/caption_image'
 
-	def genMeme(self, template_id, username, password, text0, text1, font="impact", max_font_size="50", boxes={}):
-		payload=dict([('template_id',template_id), ('username',username),
-		('password',password),('text0',text0),('text1',text1),
-		('font',font),('max_font_size',max_font_size),
-		('boxes',boxes)])
-		results = requests.post(self.CaptionImageApi,params=payload)
-		data = results.json()
-		return(data["data"]["url"])
+    def genMeme(self, template_id, username, password, text0, text1, font="impact", max_font_size="50", boxes={}):
+            payload=dict([('template_id',template_id), ('username',username),
+            ('password',password),('text0',text0),('text1',text1),
+            ('font',font),('max_font_size',max_font_size),
+            ('boxes',boxes)])
+            results = requests.post(self.CaptionImageApi,params=payload)
+            data = results.json()
+            if(data["success"] == True):
+                return(data["data"]["url"])
+            else:
+                return(data["error_message"])
+    def getAvailableMemes(self):
+        results = requests.get(self.GetMemesApi)
+        return results.json())
